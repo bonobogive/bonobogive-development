b2e8a68fa9d40d93f45bad680afcc5394261e0ef
Gareth Duncan <gareth.duncan87@gmail.com>
2020-04-26 21:42:52 +0100

    first deploy

---
+++ assets/settings/appsettings.Production.js
@@ -0,0 +1,8 @@
+(function (window) {
+    window.__config = window.__config || {};
+  
+    window.__config.bonobogiveApiUrl = 'https://o039bbdygl.execute-api.eu-west-2.amazonaws.com/production';
+    window.__config.bonobogiveWebsocketUrl = "wss://r8tw7tdnvj.execute-api.eu-west-2.amazonaws.com/production"
+
+  }(this));
+  
---
+++ assets/settings/appsettings.js
@@ -0,0 +1,9 @@
+(function (window) {
+    window.__config = window.__config || {};
+  
+    window.__config.bonobogiveApiUrl = 'https://localhost:44322';
+    window.__config.bonobogiveWebsocketUrl = "wss://r8tw7tdnvj.execute-api.eu-west-2.amazonaws.com/development"
+    // window.__config.bonobogiveApiUrl = 'https://w0dczgnww7.execute-api.eu-west-2.amazonaws.com/development';
+
+  }(this));
+  
